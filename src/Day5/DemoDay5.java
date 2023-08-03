cd4d0b7ab9d71ee3de3e1bd7033e85c851e11de8
sukhvir75 <140761481+sukhvir75@users.noreply.github.com>
2023-08-02 23:02:35 -0400

    classDemoDay5

---
+++ src/Day5/DemoDay5.java
@@ -0,0 +1,15 @@
+package Day5;
+
+public class DemoDay5 {
+	public static void main(String args []) {
+		System.out.println("Today is sunny...");
+		
+	}
+	public static void getdata() {
+		System.out.println("this is a Data");
+	}
+	public static void getInfo() {
+		System.out.println("Today is java Day");
+	}
+
+}
